1e080d8ace126268adfb257457a0bb25880f336c
iqbal393 <92832228+iqbal393@users.noreply.github.com>
2024-10-28 09:15:30 +0700

    menambahkan fungsi hapus, ubah dan pencarian

---
+++ belajar/pertemuan3/hack.php
@@ -0,0 +1,1 @@
+<h1 style="position: absolute; top: 0; left: 0; right:0; bottom: 0; background-color: black; color: red; font-size: 100px; margin: 0; padding:0;">HAHAHA ANDA DI HACK!!!</h1>
---
+++ belajar/pertemuan3/index.php
@@ -0,0 +1,60 @@
+<?php
+require 'functions.php';
+// Koneksi ke Database & Pilih Database
+// Query isi tabel mahasiswa
+// ubah data ke dalam array
+// $row = mysqli_fetch_row($result); // array numerik
+// $row = mysqli_fetch_assoc($result); // array associative
+// $row = mysqli_fetch_array($result); // keduanya
+// tampung ke variabel mahasiswa
+$mahasiswa = query("SELECT * FROM mahasiswa");
+// ketika tombol cari diklik
+if(isset($_POST['cari'])) {
+  $mahasiswa = cari($_POST['keyword']);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+<body>
+  <h3>Daftar Mahasiswa</h3>
+  <a href="tambah.php">Tambah Data Mahasiswa</a>
+  <br><br>
+  <form action="" method="post">
+      <input type="text" name="keyword" size="40" placeholder="masukkan keyword pencarian.." autocomplete="off" autofocus>
+      <button type="submit" name="cari">Cari!</button>
+  </form>
+  <br>
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>Nama</th>
+      <th>Aksi</th>
+    </tr>
+    <?php if(empty($mahasiswa)) : ?>
+    <tr>
+      <td colspan="4">
+        <p style="color: red; font-style: italic;">data mahasiswa tidak ditemukan!</p>
+      </td>
+    </tr>
+    <?php endif; ?>
+    <?php $i = 1;
+    foreach($mahasiswa as $m) : ?>
+    <tr>
+      <td><?= $i++; ?></td>
+      <td><img src="img/<?= $m['gambar']; ?>" width="60"></td>
+      <td><?= $m['nama']; ?></td>
+      <td>
+        <a href="detail.php?id=<?= $m['id']; ?>">lihat detail</a>
+
+      </td>
+    </tr>
+    <?php endforeach; ?>
+  </table>
+</body>
+</html>
